2fe122850270f27b8364e1c7517ba3668d354d39
Patrick Sweeney <plsweeney25@gmail.com>
2019-12-08 12:18:04 -0500

    Added error printing

--- Interpreter.java
+++ Interpreter.java
@@ -21,10 +21,12 @@ import java.util.Map;
 class Interpreter{
     
     private Map<String,Integer> symbolTable = new HashMap<String, Integer>();
+    private Map<String,String> grammar = new HashMap<String,String>();
     
     Interpreter(String source) throws Exception {
         Map<String,Integer> symbolTable = new HashMap<String, Integer>();
-
+        Map<String,String> grammar = new HashMap<String,String>();
+        buildGrammar();
         try {
             SynParser parser = new SynParser(source);
             SynParser.Node program = parser.program();
@@ -34,16 +36,33 @@ class Interpreter{
         }
 
     }
+    // builds grammar for use with error statements
+    void buildGrammar(){
+        grammar.put("program", "function id ( ) <block> end");
+        grammar.put("block", "<statement> | <statement> <block>");
+        grammar.put("statement", "<if_statement> | <assignment_statement> | <while_statement> | <print_statement> | <for_statement>");
+        grammar.put("if_statement", "if <boolean_expression>  <block> else <block> end");
+        grammar.put("while_statement", "while <boolean_expression> <block> end");
+        grammar.put("assignment_statement", "id <assignment_operator> <arithmetic_expression>");
+        grammar.put("for_statement", "for id = <iter> <block> end");
+        grammar.put("print_statement", "print ( <arithmetic_expression> )");
+        grammar.put("iter", "<arithmetic_expression> : <arithmetic_expression>");
+        grammar.put("boolean_expression", "<relative_op> <arithmetic_expression> <arithmetic_expression>");
+        grammar.put("relative_op", "le_operator | lt_operator | ge_operator | gt_operator | eq_operator | ne_operator");
+        grammar.put("arithmetic_expression", "<id> | <literal_integer> | <binary_expression");
+        grammar.put("binary_expression", "<arithmetic_op> <arithmetic_expression> <arithmetic_expression>");
+        grammar.put("arithmetic_op", "add_operator | sub_operator | mul_operator | div_operator | mod_operator | exp_operator | rev_div_operator");
+    }
 
-    void interpret(SynParser.Node program) {
+    void interpret(SynParser.Node program) throws Exception{
         // in an error-free AST, only block nodes should be
         // present as children of the program, so no need to check
 
         ArrayList<SynParser.Node> children = program.getChildren();
         block(children.get(4));
     }
-
-    void block(SynParser.Node block) {
+    // implements block
+    void block(SynParser.Node block)  throws Exception{
         // in an error-free AST, only statement nodes, possibly followed 
         // by a block, should be present as children of the block so no 
         // need to check
@@ -52,8 +71,29 @@ class Interpreter{
         statement(children.get(0));
         if (children.size() > 1) block(children.get(1));
     }
-
-    void statement(SynParser.Node statement) {
+    // Throws an exception on the error node provided
+    void throwError(SynParser.Node errorNode) throws Exception{
+        Exception ex = new Exception("Expected " + errorNode.getErrorToken() + " at line " + errorNode.getErrorLine() + " token " + errorNode.getErrorPos() + ".\nCorrect grammar for " + errorNode.getNodeType() + " is " + grammar.get(errorNode.getNodeType()) + ".\n");
+        throw ex;
+    }
+    // Throws custom exception for missing ids
+    void throwMissingID(SynParser.Node errorID) throws Exception{
+        Exception ex = new Exception("Missing symbol " + errorID.getNodeValue());
+        throw ex;
+    }
+    // implements statement
+    void statement(SynParser.Node statement) throws Exception {
+         // if the node is the source of an error then raise exception
+        if (statement.getError() == 2) {
+            throwError(statement);
+        }else if(statement.getError() == 1){
+            ArrayList<SynParser.Node> children = statement.getChildren();
+            for (SynParser.Node x : children) {
+                if(x.getError() == 2){
+                    throwError(x);
+                }
+            }
+        }
         SynParser.Node child = statement.getChildren().get(0); 
         switch (child.getNodeType()) {
             case "print_statement":
@@ -67,12 +107,12 @@ class Interpreter{
                 break;
         }
     }
-
-    void printStatement(SynParser.Node print) {
+    // implements print statement
+    void printStatement(SynParser.Node print) throws Exception {
         System.out.println(calculateValue(print.getChildren().get(2)));
     }
-    
-    void assignStatement(SynParser.Node assign){
+    // implements assignment statement
+    void assignStatement(SynParser.Node assign) throws Exception{
         //Get variable name
         String var_name = assign.getChildren().get(0).getNodeValue();
         //Get value of arithmetic expression
@@ -81,8 +121,8 @@ class Interpreter{
         symbolTable.put(var_name, value);
     }
 
-    //Calculates new values for assignment statements
-    int calculateValue(SynParser.Node n){
+    // implements arithmentic expression
+    int calculateValue(SynParser.Node n) throws Exception{
         Queue<SynParser.Node> q = new LinkedList<>();
         ArrayList<SynParser.Node> children;
         int[] values = new int[2];
@@ -103,12 +143,16 @@ class Interpreter{
 
         while(q.size() > 0){
             node = q.remove();
-
             // inspect node contents
             if(node.getNodeType().equals("arithmetic_op")){
                 opType = node.getNodeValue();
             }else if(node.getNodeType().equals("identifier")){
-                values[index++] = symbolTable.get(node.getNodeValue());
+                // check if symbol exists before assigning it to operand array
+                if(symbolTable.containsKey(node.getNodeValue())){
+                    values[index++] = symbolTable.get(node.getNodeValue());
+                }else{
+                    throwMissingID(node);
+                }
             }else if(node.getNodeType().equals("integer_lt")){
                 values[index++] = Integer.parseInt(node.getNodeValue());
             // if node is a binary expression, add its children to the queue
@@ -135,7 +179,7 @@ class Interpreter{
         return result;
     }
 
-    // Performs the appropriate arrithmetic calculation
+    // Performs the appropriate arithmetic calculation
     int performCalc(String operator, int[] operand, int index){
         int result = 0;
         
